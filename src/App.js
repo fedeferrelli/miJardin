c24c7175a28acb98f459503a6876ee185884b77e
Fede <ferrelli.mazza@gmail.com>
2022-11-20 08:26:51 -0300

    router 2

--- src/App.js
+++ src/App.js
@@ -1,61 +1,67 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  useParams,
+} from "react-router-dom";
 
+import Navigation from "./pages/Navigation";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Servicios from "./pages/Servicios";
+import Contacto from "./pages/Contacto";
+import Footer from "./pages/Footer";
 
+//const manes = [{name:'keith', edad: 45}, {name:'mick', edad: 40}, {name:'ron', edad: 5}]
 
-import { BrowserRouter as Router, Route, Routes, Link, useParams } from 'react-router-dom';
+import manes from "./data";
 
-import Navigation from './pages/Navigation'
-import Home from './pages/Home';
-import About from './pages/About';
-import Servicios from './pages/Servicios';
-import Contacto from './pages/Contacto';
-import Footer from './pages/Footer';
+console.log(manes);
 
-
-const manes = [{name:'keith', edad: 45}, {name:'mick', edad: 40}, {name:'ron', edad: 5}]
-
-
-const Fede = () =>{
+const Fede = () => {
   return (
     <>
-    <h1>fede</h1>
+      <h1>fede</h1>
 
-    {manes.map(man=>(
-      <li key={man.name}><Link to={`/manes/${man.name}`} >{man.name}</Link></li>
-    ))}
+      {manes.map((man) => (
+        <li key={man.name}>
+          <Link to={`/manes/${man.name}`}>{man.name}</Link>
+        </li>
+      ))}
 
-    <Link to="/what">ir a what</Link>
+      <Link to="/what">ir a what</Link>
     </>
-  )
-} 
+  );
+};
 
-const What = () =>{
-  const {nombre} = useParams()
-  console.log(nombre)
+const What = () => {
+  const { nombre } = useParams();
+  console.log(nombre);
+
+  const dataToPrint = manes.find((man) => man.name === nombre);
+  const { name, edad } = dataToPrint;
+  console.log(dataToPrint);
+
   return (
     <>
-    <h1>{nombre}</h1>
-    <Link to="/">ir a home</Link>
+      <h1>{name}</h1>
+      <h1>{edad}</h1>
+
+      <Link to="/">ir a home</Link>
     </>
-  )
-} 
+  );
+};
 
 function App() {
   return (
-
     <Router>
-
       <Routes>
-
-      <Route path='/'  element={<Fede/>}  />
-      <Route path='/manes/:nombre'  element={<What/>}  />
-
+        <Route path="/" element={<Fede />} />
+        <Route path="/manes/:nombre" element={<What />} />
       </Routes>
-      
 
-
-
-
-   {/*    <Navigation />
+      {/*    <Navigation />
 
       <Home  />
 
@@ -69,7 +75,6 @@ function App() {
 
  */}
     </Router>
-
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,3 @@
+const data = [{name:'keith', edad: 45}, {name:'mick', edad: 40}, {name:'ron', edad: 5}]
+
+export default data
